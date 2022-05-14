d36ff2c3818e0db74d331a54439bc5db8fb14f5c
Nelvin Mathew <nelvinmathew14@gmail.com>
2022-05-14 14:08:19 +0530

    update

--- app/src/main/java/com/huawei/medassist/loginpage.java
+++ app/src/main/java/com/huawei/medassist/loginpage.java
@@ -2,7 +2,10 @@ package com.huawei.medassist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class loginpage extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class loginpage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loginpage);
+
+        }
+    public void goToActivity(View view){
+        Intent intent = new Intent (this, MainActivity.class);
+        startActivity(intent);
     }
 }
